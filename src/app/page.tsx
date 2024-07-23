7ef97927a2e178624136737d62a7464a1a7617cc
vigneshwaranmanikandan <vigneshwaranms34@gmail.com@gmail.com>
2024-07-23 12:26:46 +0530

    cleared

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,10 +14,9 @@ import Services from "@/components/Services";
 import { getAllPosts } from "@/utils/markdown";
 import { Metadata } from "next";
 
-
 export const metadata: Metadata = {
-  title: "Play Next.js - SaaS Starter Kit and Boilerplate for Next.js",
-  description: "Free Next.js SaaS Boilerplate and Starter Kit designed and built for SaaS startups. It comes with all necessary integrations, pages, and components you need to launch a feature-rich SaaS websites.",
+  title: "Asridhi Infotech",
+  description: "Infinite in Talent Recruitment",
 };
 
 export default function Home() {
@@ -31,12 +30,11 @@ export default function Home() {
       <About />
       <CallToAction />
       <Pricing />
-      <Reason/>
-      <Services/>
+      <Reason />
+      <Services />
       <Contact />
+      <Faq />
       <HomeBlogSection posts={posts} />
-      
-      
     </main>
   );
 }
---
+++ src/components/Pricing/index.tsx
@@ -0,0 +1,13 @@
+"use client";
+import SectionTitle from "../Common/SectionTitle";
+import PricingBox from "./PricingBox";
+import { pricingData } from "@/stripe/pricingData";
+
+const Pricing = () => {
+  return (
+    <section>
+    </section>
+  );
+};
+
+export default Pricing;
